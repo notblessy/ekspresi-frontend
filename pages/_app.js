32ec09fe335c4e234ef9e1e9ca99217e6f4e6202
Frederich Blessy <frederichblessy@gmail.com>
2020-10-23 15:53:57 +0700

    Add meta viewport

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,18 @@
 import { ThemeProvider, CSSReset } from "@chakra-ui/core";
-
+import Head from "next/head";
 import { SWRConfig } from "swr";
 import fetch from "../libs/fetch.js";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+        <title>Ekspresi - Todo List</title>
+      </Head>
       <CSSReset />
       <SWRConfig
         value={{
